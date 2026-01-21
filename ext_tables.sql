a75064e38c212f6c81a170be6cdb52437f5f3a97
Michael Kettel <mkettel@gmail.com>
2026-01-21 14:17:10 +0100

    Release v1.0.1

---
+++ ext_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tt_content (
+    aisearch_url_hash VARCHAR(255) DEFAULT '' NOT NULL,
+    aisearch_api_host VARCHAR(255) DEFAULT 'https://chat.rm-solutions.de' NOT NULL,
+    aisearch_prompt_template VARCHAR(255) DEFAULT 'ai_search' NOT NULL,
+    aisearch_list_all_sources TINYINT(3) DEFAULT 0 NOT NULL,
+);
